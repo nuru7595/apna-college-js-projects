009e8c611f9046caacadb273b6495a232bb99aa7
Nuru <nuruzzamannuru7595@gmail.com>
2024-10-03 00:55:37 +0600

    Level 3

--- main.js
+++ main.js
@@ -78,8 +78,46 @@ l2q2Btn.addEventListener("click", () => {
         l2q2Result.innerHTML = "You have got: D";
     } else if (l2q2Num >= 0 && l2q2Num <= 32) {
         l2q2Result.innerHTML = "You have got: F";
-    }else {
+    } else {
         l2q2Result.innerHTML = "Please enter a valid mark";
     }
 });
 // Grade Calculator;
+const l3q1Btn = document.getElementById("l3q1Btn");
+const l3q1Result = document.getElementById("l3q1Result");
+l3q1Btn.addEventListener("click", () => {
+    l3q1Result.innerText = "";
+    let l3q1Num = document.getElementById("l3q1Input").value;
+    l3q1Num = Number(l3q1Num);
+    if (l3q1Num >= 0 && l3q1Num <= 200) {
+        for (let i = 0; i <= l3q1Num; i++) {
+            if (i % 2 == 0) {
+                l3q1Result.append(i + ", ");
+            }
+        }
+    } else {
+        l3q1Result.innerText = "Please enter a number between 0 - 200";
+    }
+});
+// Even number genarator;
+const l3q2Btn = document.getElementById("l3q2Btn");
+const l3q2Result = document.getElementById("l3q2Result");
+l3q2Btn.addEventListener("click", () => {
+    let l3q2Num = prompt("Enter a number...");
+    l3q2Num = Number(l3q2Num);
+    const x = 7;
+    let i;
+    for(i = 1; l3q2Num != x; i++){
+        l3q2Num = prompt("Attempt no " + i + " failed! Try again.");
+    }
+    if (i == 1) {
+        l3q2Result.innerText = "Correct! you did it in 1st try!";
+    } else if (i == 2){
+        l3q2Result.innerText = "Correct! you did it in 2nd try!";
+    } else if (i == 3) {
+        l3q2Result.innerText = "Correct! you did it in 3rd try!";
+    } else {
+        l3q2Result.innerText = "Correct! you did it in " + i + "th try!";
+    }
+});
+// Nuru's favorite number;
